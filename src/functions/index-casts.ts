dabb272af100eecd2c7f3b5eca91938900580002
Satyajeet Pal <psatyajeet@gmail.com>
2023-03-11 13:59:33 -0500

    Ignore some tags

--- src/functions/index-casts.ts
+++ src/functions/index-casts.ts
@@ -191,6 +191,7 @@ function cleanCasts(casts: Cast[]): Cast[] {
 }
 
 export function getAllTags(casts: Cast[]): CastTag[] {
+  const TAGS_TO_IGNORE = ['what', 'things', 'did', 'post']
   const cleanedTags: CastTag[] = new Array()
 
   for (const cast of casts) {
@@ -204,10 +205,11 @@ export function getAllTags(casts: Cast[]): CastTag[] {
     if (!tags) {
       continue
     } else {
-      const processedTags = new Set<string>()
+      const processedTags = new Set<string>(TAGS_TO_IGNORE)
       for (const tag of tags) {
-        const lowerCaseTag = tag.toLowerCase()
-        if (tag.length <= 2) {
+        const cleanedTag = tag.replaceAll('#', '').trim();
+        const lowerCaseTag = cleanedTag.toLowerCase()
+        if (cleanedTag.length <= 2) {
           continue // Skip tags that are just a single character
         }
         if (processedTags.has(lowerCaseTag)) {
@@ -215,7 +217,7 @@ export function getAllTags(casts: Cast[]): CastTag[] {
         }
         cleanedTags.push({
           cast_hash: cast.hash,
-          tag: tag.replaceAll('#', '').trim(),
+          tag: cleanedTag,
           implicit: false,
         })
         processedTags.add(lowerCaseTag)
